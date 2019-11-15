68d0fd6288eb668667e76722a0d0e841c1339f39
Conor Johnston <cnrswim@yahoo.com>
2019-11-15 10:37:05 -0500

    created stock component

---
+++ src/components/Stock.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Stock extends Component {
+
+    componentDidMount() {
+
+    }
+    
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
