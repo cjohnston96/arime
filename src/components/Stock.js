0543a4d4a79faefeec5c219b96c7b747b1f6b3c6
Conor Johnston <cnrswim@yahoo.com>
2019-11-15 11:33:44 -0500

    fetching data

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Stock extends Component {
+export default class Stock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            stocks: []
+        }
+    }
 
     componentDidMount() {
-
+        fetch("https://api-project-backend.herokuapp.com/Apple")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                this.setState({
+                    isLoaded: true,
+                    stocks: result.stocks
+                });
+                },
+                (error) => {
+                    this.setState({
+                    isLoaded: true,
+                    error
+                    });
+                }
+            )
     }
-    
+
     render() {
+        const { error, isLoaded, stocks } = this.state;
+        if(error){
+            return(
+                <div>Error: {error.message}</div>
+            )
+        } else if (!isLoaded) {
+            return(
+                <div>Fetching data...</div>
+            )
+        } else {
         return(
-            <div></div>
+            <ul>
+                {stocks.map(stock =>(
+                    <li key = {stock.name}>
+                        {stock.name}
+                        {stock.data}
+                    </li>
+                ))}
+            </ul>
         );
+        }
     }
 }
